bac35bd1ed4cf274b96487958dd155c74d09a2bf
kenykenoei <matheuschavier112004@gmail.com>
2021-06-02 15:55:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Matheus"
+let age = 17
+let currentYear = 2021
+console.log("eu nasci no ano de "+ (currentYear-age))
+let souHumano = true
+let number1 = 3
+let number2 = 2
+console.log(number1+number2)
